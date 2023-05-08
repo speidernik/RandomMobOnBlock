4a31f018cb3a81fe24535a4a2375cab4b07d6dc5
Niklas Bürgel <Speidernik@gmail.com>
2023-05-09 01:57:40 +0200

    Works now

--- src/main/java/org/nicemods/randomblock/PlayerDeathListener.java
+++ src/main/java/org/nicemods/randomblock/PlayerDeathListener.java
@@ -1,152 +1,107 @@
 package org.nicemods.randomblock;
 
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.WorldType;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.ArrayList;
-import java.util.List;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.PlayerInventory;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.bukkit.Bukkit.getServer;
 
 public class PlayerDeathListener implements Listener {
 
     private final RandomBlock plugin;
+    public Set<UUID> playersToClearInventory;
+    public Set<UUID> allPlayers;
+    Location newSpawnLocation;
 
     public PlayerDeathListener(RandomBlock plugin) {
         this.plugin = plugin;
+        this.playersToClearInventory = new HashSet<>();
+        this.allPlayers = new HashSet<>();
     }
 
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        plugin.cleanList();
+        newSpawnLocation = getNewSpawnLocation();
+
+        for (UUID uuid : allPlayers) {
+            Player player = Bukkit.getPlayer(uuid);
+
+            // If the player is not online, add their UUID to playersToClearInventory
+            assert player != null;
+            if (!player.isOnline()) {
+                playersToClearInventory.add(uuid);
+                continue;
+            }
+            if (!isPlayerAlive(player)) continue;
+
+            resetPlayer(player);
+        }
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        World currentWorld = plugin.getServer().getWorlds().get(0);
-        player.teleport(currentWorld.getSpawnLocation());
+        if (playersToClearInventory.contains(player.getUniqueId())) {
+            resetPlayer(player);
+            playersToClearInventory.remove(player.getUniqueId());
+        }
+        allPlayers.add(player.getUniqueId());
     }
-
 
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        // Pause time in the current world
-        plugin.cleanList();
-        World currentWorld = plugin.getServer().getWorlds().get(0);
-        currentWorld.setGameRuleValue("doDaylightCycle", "false");
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        resetPlayer(player);
+    }
 
-        // Change players' game mode to spectator
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.setGameMode(GameMode.SPECTATOR);
+    private void resetPlayer(Player player) {
+        player.setBedSpawnLocation(newSpawnLocation, true);
+        player.teleport(newSpawnLocation);
+        AttributeInstance maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+        if (maxHealth != null) {
+            player.setHealth(maxHealth.getValue());
         }
-
-        // Generate new worlds asynchronously
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            String newWorldName = "new_world_" + System.currentTimeMillis();
-
-            Bukkit.getScheduler().runTask(plugin, () -> {
-                World newOverworld = generateNewWorld(newWorldName, World.Environment.NORMAL);
-                World newNether = generateNewWorld(newWorldName + "_nether", World.Environment.NETHER);
-                World newEnd = generateNewWorld(newWorldName + "_the_end", World.Environment.THE_END);
-
-                // Set the server's default spawn point to the new world
-                plugin.getServer().getWorlds().get(0).setSpawnLocation(newOverworld.getSpawnLocation());
-
-                // Update the server.properties file to use the new world as the default world
-                updateServerProperties("level-name", newWorldName);
-
-                // Teleport players to the new world and change their game mode
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    player.teleport(newOverworld.getSpawnLocation());
-                    player.setGameMode(GameMode.SURVIVAL);
-                    player.setAllowFlight(false);
-                    player.setBedSpawnLocation(newOverworld.getSpawnLocation(), true);
-
-                    // Set player's health and food level to maximum
-                    player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
-                    player.setFoodLevel(20);
-                }
-
-                // Unload the old overworld, Nether, and End
-                List<World> oldWorlds = new ArrayList<>(Bukkit.getWorlds());
-                for (World oldWorld : oldWorlds) {
-                    String oldWorldName = oldWorld.getName();
-                    if (oldWorldName.equals(currentWorld.getName()) || oldWorldName.equals(currentWorld.getName() + "_nether") || oldWorldName.equals(currentWorld.getName() + "_the_end")) {
-                        Bukkit.unloadWorld(oldWorldName, true);
-                    }
-                }
-
-                // Delete the old world folders
-                for (World oldWorld : oldWorlds) {
-                    String oldWorldName = oldWorld.getName();
-                    if (oldWorldName.equals(currentWorld.getName()) || oldWorldName.equals(currentWorld.getName() + "_nether") || oldWorldName.equals(currentWorld.getName() + "_the_end")) {
-                        File oldWorldFolder = new File(plugin.getDataFolder().getParentFile().getParentFile(), oldWorldName);
-                        deleteDirectory(oldWorldFolder);
-                    }
-                }
-
-                // Resume time in the new world
-                newOverworld.setGameRuleValue("doDaylightCycle", "true");
-                newNether.setGameRuleValue("doDaylightCycle", "true");
-                newEnd.setGameRuleValue("doDaylightCycle", "true");
-            });
-        });
+        player.setFoodLevel(20);
+        PlayerInventory inventory = player.getInventory();
+        inventory.clear();
+        player.setLevel(0);
+        player.setExp(0);
     }
 
-
-
-    private World generateNewWorld(String worldName, World.Environment environment) {
-        WorldCreator worldCreator = new WorldCreator(worldName);
-        worldCreator.environment(environment);
-        worldCreator.type(WorldType.NORMAL);
-        worldCreator.generateStructures(true);
-        worldCreator.seed(System.currentTimeMillis());
-        return Bukkit.createWorld(worldCreator);
+    public boolean isPlayerAlive(Player player) {
+        return player.getHealth() > 0;
     }
 
+    private Location getNewSpawnLocation() {
+        World world = getServer().getWorlds().get(0);
+        Location currentSpawnLocation = world.getSpawnLocation();
 
-    public void updateServerProperties(String key, String value) {
-        Properties properties = new Properties();
-        File propertiesFile = new File("server.properties");
 
-        try {
-            // Load the server.properties file
-            FileInputStream fileInputStream = new FileInputStream(propertiesFile);
-            properties.load(fileInputStream);
-            fileInputStream.close();
+        // 10,000 blocks north (negative Z direction) from the current spawn location
+        double x = currentSpawnLocation.getX();
+        double z = currentSpawnLocation.getZ() - 10000;
 
-            // Modify the value of the specified key
-            properties.setProperty(key, value);
+        // Get the highest block Y-coordinate at the new spawn location
+        int y = world.getHighestBlockYAt((int) x, (int) z);
 
-            // Save the modified server.properties file
-            FileOutputStream fileOutputStream = new FileOutputStream(propertiesFile);
-            properties.store(fileOutputStream, "Minecraft server properties");
-            fileOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Set the new spawn location one block above the highest block
+        Location newSpawnLocation = new Location(world, x, y + 1, z);
+        world.setSpawnLocation(newSpawnLocation);
+        return newSpawnLocation;
+
     }
-
-    public static boolean deleteDirectory(File directory) {
-        if (directory.isDirectory()) {
-            String[] children = directory.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDirectory(new File(directory, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-        return directory.delete();
-    }
-
 }
--- src/main/java/org/nicemods/randomblock/RandomBlock.java
+++ src/main/java/org/nicemods/randomblock/RandomBlock.java
@@ -4,29 +4,40 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
 
 public class RandomBlock extends JavaPlugin implements Listener {
 
+    private final HashMap<String, EntityType> blockMobMap = new HashMap<>();
+    private final Random random = new Random();
+    private final PlayerDeathListener playerDeathListener;
+    private EntityType[] entityTypes;
 
-
-    private HashMap<String, EntityType> blockMobMap = new HashMap<>();
-    private Random random = new Random();
+    public RandomBlock() {
+        this.playerDeathListener = new PlayerDeathListener(this);
+    }
 
     public void cleanList() {
         blockMobMap.clear();
     }
+
     @Override
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
         getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
+        playerDeathListener.playersToClearInventory = new HashSet<>();
+        playerDeathListener.allPlayers = new HashSet<>();
+    }
+
+    private void initializeEntityTypes() {
+        entityTypes = EntityType.values();
     }
 
     @EventHandler
@@ -38,19 +49,26 @@ public class RandomBlock extends JavaPlugin implements Listener {
         if (blockMobMap.containsKey(blockType)) {
             entityType = blockMobMap.get(blockType);
         } else {
-            EntityType[] entityTypes = EntityType.values();
+            if (entityTypes == null) {
+                initializeEntityTypes();
+            }
             int randomIndex = random.nextInt(entityTypes.length);
-
             entityType = entityTypes[randomIndex];
-
-            while (!entityType.isAlive() || !entityType.isSpawnable()) {
+            int count = 0;
+            while ((!entityType.isAlive() || !entityType.isSpawnable()) && count < 100) {
                 randomIndex = random.nextInt(entityTypes.length);
                 entityType = entityTypes[randomIndex];
+                count++;
             }
             blockMobMap.put(blockType, entityType);
             Bukkit.broadcastMessage(ChatColor.GREEN + "From now on, breaking " + blockType + " blocks will spawn " + entityType.name() + "s.");
         }
 
-        LivingEntity spawnedMob = (LivingEntity) block.getWorld().spawnEntity(block.getLocation().add(0.5, 0.5, 0.5), entityType);
+        if (entityType.isAlive() && entityType.isSpawnable()) {
+            assert entityType.getEntityClass() != null;
+            block.getWorld().spawn(block.getLocation().add(0.5, 0.5, 0.5), entityType.getEntityClass());
+        } else {
+            getLogger().warning("Could not spawn entity of type " + entityType.name() + " because it is not spawnable or alive.");
+        }
     }
 }
